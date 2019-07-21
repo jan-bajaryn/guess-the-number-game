056074d8c8bacd397c7882f558e8eadd27222f78
Jan <jabajaryn@gmail.com>
2019-07-21 07:08:14 +0300

    try 7

--- web/src/main/java/academy/learnprogramming/controller/GameController.java
+++ web/src/main/java/academy/learnprogramming/controller/GameController.java
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.engine.AttributeName;
 
 @Controller
 @Slf4j
@@ -42,4 +42,36 @@ public class GameController {
         return GameMappings.REDIRECT_PLAY;
     }
 
+    @GetMapping(GameMappings.RESTART)
+    public String restart(Model model){
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        gameService.reset();
+        return GameMappings.REDIRECT_PLAY;
+    }
+
+    @GetMapping(GameMappings.HOME)
+    public String home(){
+        gameService.reset();
+        return GameMappings.HOME;
+    }
+
 }
+//          -Create a Thymeleaf template for game-over view (name it game-over.html)
+//        -In the html file add a centered div tag that contains
+//        -game title in h1 tag e.g. text gGuess The Number Gameh.
+//        -text gGame Overh in a h2 tag.
+//        -Result message in a h3 tag (model attribute).
+//        -A h3 tag with a tag (anchor) with text gPlay Againh. Clicking on Play Again link should call the controller restart method to restart the game.
+//        -A h3 tag with a tag (anchor) with text Home. Clicking on the Home link should open the home page (home.html).
+//        -The footer tag - same as in the play.html file (You can copy paste it).
+//
+//        -Add the method restart to GameController
+//        -Method restart() should call gameService reset() method to reset the game and redirect to play view. It needs to be mapped to /restart.
+//
+//        -Check the logic is working correctly for the game
+//        -After game is won or lost game-over view should be displayed.
+//        -Clicking on Home link should show the home view.
+//
+//        TIPS:
+//        -Use constants
+//        -Use template preprocessing tags
